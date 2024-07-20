8d91fb7e3979896b205f1ab2763ad1e5ea7fb27d
Mohammed Essa <moessa089@gmail.com>
2024-07-20 16:58:09 +0000

    Install gunicorn and ammend settings file

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6dsqttsfv%#tt)iak#@eio*$atmr7kd6(^9++3b%387iloru&m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-moessa1-djangoproject-aylwgsltu16.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-moessa1-djangoproject-aylwgsltu16.ws.codeinstitute-ide.net',
+'.herokuapp.com']
 
 
 # Application definition
